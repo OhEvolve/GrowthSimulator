8cd115cbea96928c70de6f95499c84c6ef6ee171
Pandyr <hole0077@umn.edu>
2019-03-10 21:32:23 -0400

    initial commit

---
+++ drug_profiles.py
@@ -0,0 +1,16 @@
+
+def linear_profile(nx,ny):
+    """ Create a drug profile that peaks in middle """
+    l = np.arange(0,nx)
+    x = np.repeat(l,ny).reshape(nx,ny,1)
+    x = ((nx - 1)/2 - abs(x - (nx - 1)/2))/((nx-1)/2)
+    return x
+
+def stepwise_profile(nx,ny,steps = 6):
+    """ Create a drug profile that peaks in middle """
+    l = np.arange(0,nx)
+    x = np.repeat(l,ny).reshape(nx,ny,1)
+    x = ((nx - 1)/2 - abs(x - (nx - 1)/2))
+    x,_ = np.divmod(x,nx/(2*steps))
+    x /= np.max(x)
+    return x
